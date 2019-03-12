6f87fe84dc564cd546bc455b9c654a49f62f233c
MJeremy2017 <zhangyue9306@gmail.com>
2019-03-12 11:03:48 +0800

    imports

--- main/Hello_World.go
+++ main/Hello_World.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/goinaction/code/chapter3/words"
+	"io/ioutil"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Hello World")  // godoc fmt -- more info
+
+	filename := os.Args[1]
+
+	contents, err := ioutil.ReadFile(filename)
+	if err!=nil {
+		fmt.Println(err)
+		return
+	}
+
+	text := string(contents)
+	count := words.CountWords(text)
+
+	fmt.Printf("There are %d words in the txt \n", count)
+
 }
