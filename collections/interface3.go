e93734423e784696fb659bcd3c14b7b566194c30
MJeremy2017 <zhangyue9306@gmail.com>
2019-03-19 10:57:15 +0800

    embedding types

--- collections/interface3.go
+++ collections/interface3.go
@@ -1,1 +1,39 @@
 package main
+
+import "fmt"
+
+type user2 struct {
+	name string
+	email string
+}
+
+type admin2 struct {
+	user2  // embed type user
+	level string
+}
+
+type notifier2 interface {
+	notify()
+}
+
+func sendNotification2(n notifier2) {
+	n.notify()
+}
+
+func (u *user2) notify() {
+	fmt.Printf("Sending email %s <%s> \n", u.name, u.email)
+}
+
+func main()  {
+	lily := admin2{
+		user2{"lily", "lily@email.com"},
+		"super",
+	}
+
+	// can access inner type method
+	lily.notify()
+	// inner method is also promoted
+	lily.user2.notify()
+	// outer type also implement inner type interface
+	sendNotification2(&lily)
+}
