c9b2462370a1bde31a6329ef9f56ee98500e1f24
MJeremy2017 <zhangyue9306@gmail.com>
2019-03-21 10:41:14 +0800

    add goroutine

---
+++ collections/concurrency/goRoutine.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main()  {
+	// max number of logical processors
+	runtime.GOMAXPROCS(1)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	fmt.Println("Start goroutine")
+
+	// create the first goroutine
+	go func() {
+		// tell main to wait
+		defer wg.Done()
+
+		for count := 0; count < 3; count++ {
+			for char := 'a'; char < 'a'+26; char++ {
+				fmt.Printf("%c", char)
+			}
+			fmt.Println()
+		}
+		// wg.Done()
+
+	}()
+
+	// create the second goroutine
+	go func() {
+		defer wg.Done()
+
+		for count := 0; count < 3; count++ {
+			for char := 'A'; char < 'A'+26; char++ {
+				fmt.Printf("%c", char)
+			}
+			fmt.Println("")
+		}
+		//wg.Done()
+	}()
+
+	// wait the goroutine done
+	fmt.Println("Waiting ...")
+	wg.Wait()
+
+	fmt.Println("Done!")
+}
