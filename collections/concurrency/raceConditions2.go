993e0c6f950d89cbacbb7faa92dc933856c318f6
MJeremy2017 <zhangyue9306@gmail.com>
2019-03-23 17:26:12 +0800

    mutex

--- collections/concurrency/raceConditions2.go
+++ collections/concurrency/raceConditions2.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var (
+	counter2 int
+	wtg sync.WaitGroup
+	mutex sync.Mutex
+)
+
+func main() {
+	wtg.Add(2)
+
+	go incCounter2(1)
+	go incCounter2(2)
+
+	wtg.Wait()
+	fmt.Println("Final counter", counter2)
+}
+
+func incCounter2(id int) {
+	defer wtg.Done()
+
+	for count:=0; count<2; count++ {
+		mutex.Lock()  // lock the following code
+
+		value := counter2
+		runtime.Gosched()
+		value ++
+
+		counter2 = value
+
+		mutex.Unlock() // each time only 1 goroutine can access the code
+	}
+}
