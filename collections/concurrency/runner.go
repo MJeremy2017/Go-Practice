807122ca446feca1943539feaffbda10c1ca699e
MJeremy2017 <zhangyue9306@gmail.com>
2019-03-26 19:49:44 +0800

    runner

---
+++ collections/concurrency/runner.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"github.com/goinaction/code/chapter7/patterns/runner"
+	"log"
+	"os"
+	"time"
+)
+
+const timeout  = 3*time.Second
+
+func main() {
+	log.Println("Start ...")
+
+	r := runner.New(timeout)
+
+	r.Add(createTask(), createTask(), createTask())
+
+	if err := r.Start(); err != nil {
+		switch err {
+		case runner.ErrTimeout:
+			log.Println("Terminating due to timeout")
+			os.Exit(1)
+
+		case runner.ErrInterrupt:
+			log.Println("Terminating due to interruption")
+			os.Exit(2)
+		}
+
+	}
+	
+	log.Println("Process ended ...")
+}
+
+func createTask() func(int) {
+	return func(id int) {
+		log.Printf("Processing Task #%d", id)
+		time.Sleep(time.Duration(id)*time.Second)
+	}
+	
+}
